0c61f52e4ce1bd30d528cdc9d0a8b7568b1cfdfa
guofeng <liguofeng@dyrs.com.cn>
2015-08-13 17:37:28 +0800

    V 0.0.1

---
+++ src/main/java/cn/springmvc/controller/UserController.java
@@ -0,0 +1,26 @@
+package cn.springmvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.springmvc.model.User;
+import cn.springmvc.service.UserService;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+	
+	@Autowired 
+	private UserService userService;
+	
+	@RequestMapping("index")
+	public String index() {
+		User user = new User();
+		user.setNickname("guofeng");
+		user.setState(2);
+		System.out.println(userService.insertUser(user));
+		return "index";
+	}
+
+}
